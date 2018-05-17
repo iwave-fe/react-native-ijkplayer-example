d798a13e3c8b92a02d06671334b4b6454047f933
iwave-fe <n.iha@iwave-fe.net>
2018-05-17 15:04:30 +0900

    react-native update, react-native-ijkpalyer mp3 test coding.

--- App.js
+++ App.js
@@ -13,11 +13,12 @@ import {
     ProgressBarAndroid,
     Platform,
 } from 'react-native';
-import RCTIJKPlayer from 'react-native-ijkplayer';
-import RCTIJKPlayerWithController from 'react-native-ijkplayer/RCTIJKPlayerWithController';
+//import RCTIJKPlayer from 'react-native-ijkplayer';
+//import RCTIJKPlayerWithController from 'react-native-ijkplayer/RCTIJKPlayerWithController';
+import RCTIJKPlayer from 'react-native-ijkplayer/IJKPlayer';
 var {height, width} = Dimensions.get('window');
 console.log("width, height", width, height);
-import Icon from 'react-native-vector-icons/FontAwesome';
+//import Icon from 'react-native-vector-icons/FontAwesome';
 const iconSize = 120;
 
 const styles = StyleSheet.create({
@@ -44,17 +45,22 @@ export default class Example extends React.Component {
         };
     }
     componentDidMount() {
-        //let url = "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear1/prog_index.m3u8";
-        let url = "http://192.168.188.231:8080/static/1.ffconcat";
-        //let url = "http://vali-dns.cp31.ott.cibntv.net/677871F4FD937715A13F83244/03000A0E0459C2C4E07817000000018D377067-DF9D-2082-D194-820093D94914.mp4?ccode=0502&duration=390&expire=18000&psid=14070376641e6f4ab6205607837619e1&ups_client_netip=7d545025&ups_ts=1512037873&ups_userid=1130129069&utid=PGo0ErG9ZHICAXHNpcytfSLP&vid=XMTg5NDUxNDYyNA%3D%3D&vkey=A1d80b0519f959bb53fe0dbfcfeb5f306";
         // let url = "/Users/cong/Downloads/111.mov";
-        this.rctijkplayer.start({url: url});
+        // this.rctijkplayer.start({url: url});
     }
     componentWillUnmount() {
         clearInterval(this.playbackInfoUpdater);
     }
 
     render() {
+        let uri = Platform.select({
+          ios:
+            "http://www.ne.jp/asahi/music/myuu/wave/eine.mp3",
+          android:
+            "http://www.ne.jp/asahi/music/myuu/wave/eine.mp3",
+        });
+        console.log("uri => ", uri);
+        let src = {uri: uri};
         return (<View
                 style={styles.container}
             >
@@ -62,7 +68,7 @@ export default class Example extends React.Component {
                   animated
                   hidden
               />
-              <RCTIJKPlayerWithController
+              <RCTIJKPlayer
                   ref={(rctijkplayer) => {
                       this.rctijkplayer = rctijkplayer;
                   }}
@@ -71,8 +77,9 @@ export default class Example extends React.Component {
                   width={width}
                   left={0}
                   top={100}
+                  source={src}
               >
-              </RCTIJKPlayerWithController>
+              </RCTIJKPlayer>
             </View>
         );
     }
--- android/app/src/main/java/com/react_native_ijkplayer_example/MainApplication.java
+++ android/app/src/main/java/com/react_native_ijkplayer_example/MainApplication.java
@@ -11,7 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import me.yiii.RCTIJKPlayer.RCTIJKPlayerPackage;
+import com.github.iwave-fe.RCTIJKPlayer.IJKPlayerPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RCTIJKPlayerPackage()
+          new IJKPlayerPackage()
       );
     }
 
